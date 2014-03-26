2fb5d55e4ebe0586ac1462ec18aba8bedd65f287
Paolo Manca <paolo@manca.eu>
2014-03-26 01:03:30 +0100

    Removing custom post types support

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ sidebars, comments, ect.
 require_once( 'library/bones.php' );
 
 // USE THIS TEMPLATE TO CREATE CUSTOM POST TYPES EASILY
-require_once( 'library/custom-post-type.php' );
+// require_once( 'library/custom-post-type.php' );
 
 // CUSTOMIZE THE WORDPRESS ADMIN (off by default)
 // require_once( 'library/admin.php' );
