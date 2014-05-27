fa58f48b01a29bff14db8e075377d655bd75a5c8
Paolo Manca <paolo@manca.eu>
2014-05-27 23:45:32 +0200

    Messing with indentation (because I've problems)

--- functions.php
+++ functions.php
@@ -24,36 +24,36 @@ Let's get everything up and running.
 
 function bones_ahoy() {
 
-  // let's get language support going, if you need it
-  load_theme_textdomain( 'bonestheme', get_template_directory() . '/library/translation' );
+	// let's get language support going, if you need it
+	load_theme_textdomain( 'bonestheme', get_template_directory() . '/library/translation' );
 
-  // launching operation cleanup
-  add_action( 'init', 'bones_head_cleanup' );
-  // A better title
-  add_filter( 'wp_title', 'rw_title', 10, 3 );
-  // remove WP version from RSS
-  add_filter( 'the_generator', 'bones_rss_version' );
-  // remove pesky injected css for recent comments widget
-  add_filter( 'wp_head', 'bones_remove_wp_widget_recent_comments_style', 1 );
-  // clean up comment styles in the head
-  add_action( 'wp_head', 'bones_remove_recent_comments_style', 1 );
-  // clean up gallery output in wp
-  add_filter( 'gallery_style', 'bones_gallery_style' );
+	// launching operation cleanup
+	add_action( 'init', 'bones_head_cleanup' );
+	// A better title
+	add_filter( 'wp_title', 'rw_title', 10, 3 );
+	// remove WP version from RSS
+	add_filter( 'the_generator', 'bones_rss_version' );
+	// remove pesky injected css for recent comments widget
+	add_filter( 'wp_head', 'bones_remove_wp_widget_recent_comments_style', 1 );
+	// clean up comment styles in the head
+	add_action( 'wp_head', 'bones_remove_recent_comments_style', 1 );
+	// clean up gallery output in wp
+	add_filter( 'gallery_style', 'bones_gallery_style' );
 
-  // enqueue base scripts and styles
-  add_action( 'wp_enqueue_scripts', 'bones_scripts_and_styles', 999 );
-  // ie conditional wrapper
+	// enqueue base scripts and styles
+	add_action( 'wp_enqueue_scripts', 'bones_scripts_and_styles', 999 );
+	// ie conditional wrapper
 
-  // launching this stuff after theme setup
-  bones_theme_support();
+	// launching this stuff after theme setup
+	bones_theme_support();
 
-  // adding sidebars to Wordpress (these are created in functions.php)
-  add_action( 'widgets_init', 'bones_register_sidebars' );
+	// adding sidebars to Wordpress (these are created in functions.php)
+	add_action( 'widgets_init', 'bones_register_sidebars' );
 
-  // cleaning up random code around images
-  add_filter( 'the_content', 'bones_filter_ptags_on_images' );
-  // cleaning up excerpt
-  add_filter( 'excerpt_more', 'bones_excerpt_more' );
+	// cleaning up random code around images
+	add_filter( 'the_content', 'bones_filter_ptags_on_images' );
+	// cleaning up excerpt
+	add_filter( 'excerpt_more', 'bones_excerpt_more' );
 
 } /* end bones ahoy */
 
@@ -97,10 +97,10 @@ you like. Enjoy!
 add_filter( 'image_size_names_choose', 'bones_custom_image_sizes' );
 
 function bones_custom_image_sizes( $sizes ) {
-    return array_merge( $sizes, array(
-        'bones-thumb-600' => __('600px by 150px'),
-        'bones-thumb-300' => __('300px by 100px'),
-    ) );
+	return array_merge( $sizes, array(
+		'bones-thumb-600' => __('600px by 150px'),
+		'bones-thumb-300' => __('300px by 100px'),
+	));
 }
 
 /*
@@ -134,13 +134,13 @@ function bones_register_sidebars() {
 	sidebar's id is, for example:
 
 	register_sidebar(array(
-		'id' => 'sidebar2',
-		'name' => __( 'Sidebar 2', 'bonestheme' ),
-		'description' => __( 'The second (secondary) sidebar.', 'bonestheme' ),
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget' => '</div>',
-		'before_title' => '<h4 class="widgettitle">',
-		'after_title' => '</h4>',
+	'id' => 'sidebar2',
+	'name' => __( 'Sidebar 2', 'bonestheme' ),
+	'description' => __( 'The second (secondary) sidebar.', 'bonestheme' ),
+	'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	'after_widget' => '</div>',
+	'before_title' => '<h4 class="widgettitle">',
+	'after_title' => '</h4>',
 	));
 
 	To call the sidebar in your template, you can just copy
@@ -156,38 +156,38 @@ function bones_register_sidebars() {
 
 // Comment Layout
 function bones_comments( $comment, $args, $depth ) {
-   $GLOBALS['comment'] = $comment; ?>
-  <div id="comment-<?php comment_ID(); ?>" <?php comment_class('cf'); ?>>
-    <article  class="cf">
-      <header class="comment-author vcard">
-        <?php
-        /*
-          this is the new responsive optimized comment image. It used the new HTML5 data-attribute to display comment gravatars on larger screens only. What this means is that on larger posts, mobile sites don't have a ton of requests for comment images. This makes load time incredibly fast! If you'd like to change it back, just replace it with the regular wordpress gravatar call:
-          echo get_avatar($comment,$size='32',$default='<path_to_url>' );
-        */
-        ?>
-        <?php // custom gravatar call ?>
-        <?php
-          // create variable
-          $bgauthemail = get_comment_author_email();
-        ?>
-        <img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5( $bgauthemail ); ?>?s=40" class="load-gravatar avatar avatar-48 photo" height="40" width="40" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" />
-        <?php // end custom gravatar call ?>
-        <?php printf(__( '<cite class="fn">%1$s</cite> %2$s', 'bonestheme' ), get_comment_author_link(), edit_comment_link(__( '(Edit)', 'bonestheme' ),'  ','') ) ?>
-        <time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time(__( 'F jS, Y', 'bonestheme' )); ?> </a></time>
+	$GLOBALS['comment'] = $comment; ?>
+	<div id="comment-<?php comment_ID(); ?>" <?php comment_class('cf'); ?>>
+		<article  class="cf">
+			<header class="comment-author vcard">
+				<?php
+				/*
+				this is the new responsive optimized comment image. It used the new HTML5 data-attribute to display comment gravatars on larger screens only. What this means is that on larger posts, mobile sites don't have a ton of requests for comment images. This makes load time incredibly fast! If you'd like to change it back, just replace it with the regular wordpress gravatar call:
+				echo get_avatar($comment,$size='32',$default='<path_to_url>' );
+				*/
+				?>
+				<?php // custom gravatar call ?>
+				<?php
+				// create variable
+				$bgauthemail = get_comment_author_email();
+				?>
+				<img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5( $bgauthemail ); ?>?s=40" class="load-gravatar avatar avatar-48 photo" height="40" width="40" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" />
+				<?php // end custom gravatar call ?>
+				<?php printf(__( '<cite class="fn">%1$s</cite> %2$s', 'bonestheme' ), get_comment_author_link(), edit_comment_link(__( '(Edit)', 'bonestheme' ),'  ','') ) ?>
+				<time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time(__( 'F jS, Y', 'bonestheme' )); ?> </a></time>
 
-      </header>
-      <?php if ($comment->comment_approved == '0') : ?>
-        <div class="alert alert-info">
-          <p><?php _e( 'Your comment is awaiting moderation.', 'bonestheme' ) ?></p>
-        </div>
-      <?php endif; ?>
-      <section class="comment_content cf">
-        <?php comment_text() ?>
-      </section>
-      <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
-    </article>
-  <?php // </li> is added by WordPress automatically ?>
+			</header>
+			<?php if ($comment->comment_approved == '0') : ?>
+				<div class="alert alert-info">
+					<p><?php _e( 'Your comment is awaiting moderation.', 'bonestheme' ) ?></p>
+				</div>
+			<?php endif; ?>
+			<section class="comment_content cf">
+				<?php comment_text() ?>
+			</section>
+			<?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+		</article>
+<?php // </li> is added by WordPress automatically ?>
 <?php
 } // don't remove this bracket!
 
@@ -200,13 +200,12 @@ can replace these fonts, change it in your scss files
 and be up and running in seconds.
 */
 function bones_fonts() {
-  // wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
-  // wp_enqueue_style( 'googleFonts');
+	// wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
+	// wp_enqueue_style( 'googleFonts');
   
-  wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,200italic,300italic,400italic,600italic');
-  wp_enqueue_style( 'googleFonts');
+	wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,200italic,300italic,400italic,600italic');
+	wp_enqueue_style( 'googleFonts');
 }
-
 add_action('wp_print_styles', 'bones_fonts');
 
 
@@ -216,21 +215,21 @@ class marbles_walker extends Walker_Nav_Menu
 {
 	function start_lvl( &$output, $depth = 0, $args = array() ) {}
 	
-	function end_lvl( &$output, $depth = 0, $args = array() ) {}
+		function end_lvl( &$output, $depth = 0, $args = array() ) {}
 		
-	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+			function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
 								
-		$item_output = '<div class="marble">';
-		$item_output .= '<a href="'. $item->url .'" title="'. $item->url .'" rel="bookmark">';
-		$item_output .= '<div class="marble-image" role="presentation">';
-		$item_output .= get_the_post_thumbnail( $item->object_id, 'marbles-thumb' );
-		$item_output .= '</div><!-- .marble-image -->';
-		$item_output .= '<h2 class="marble-title">' . $item->title . '</h2>';
-		$item_output .= '</a>';
-		$item_output .= '</div><!-- .marble -->';
+				$item_output = '<div class="marble">';
+				$item_output .= '<a href="'. $item->url .'" title="'. $item->url .'" rel="bookmark">';
+				$item_output .= '<div class="marble-image" role="presentation">';
+				$item_output .= get_the_post_thumbnail( $item->object_id, 'marbles-thumb' );
+				$item_output .= '</div><!-- .marble-image -->';
+				$item_output .= '<h2 class="marble-title">' . $item->title . '</h2>';
+				$item_output .= '</a>';
+				$item_output .= '</div><!-- .marble -->';
 				
-		$output .= $item_output;
-	}
+				$output .= $item_output;
+			}
 }
 
 
