9270227d4973351dfb3e6943760616589528cef6
Paolo Manca <paolo@manca.eu>
2015-03-07 23:19:23 +0100

    Full width page is the default one now

--- page.php
+++ page.php
@@ -12,24 +12,22 @@
 
 get_header(); ?>
 
-	<div id="content" class="contentarea-wrap">
-		
-		<div id="inner-content" class="row">
-	
-			<main id="main" class="site-main small-12 medium-8 large-9 columns clearfix" role="main">
-		
-				<?php while ( have_posts() ) { the_post();
-	
-					get_template_part( 'tplparts/content', 'page' );
-	
-				} //endwhile; // end of the loop. ?>
-	
-			</main><!-- #main.site-main -->
-	
-			<?php get_sidebar(); ?>
-			
-		</div> <!-- end #inner-content -->
-	
-	</div> <!-- end #content.contentarea-wrap -->
+<div id="content" class="contentarea-wrap">
+
+	<div id="inner-content" class="row">
+
+		<main id="main" class="site-main small-12 columns clearfix" role="main">
+
+			<?php while ( have_posts() ) { the_post();
+
+				get_template_part( 'tplparts/content', 'page' );
+
+			} //endwhile; // end of the loop. ?>
+
+		</main><!-- #main.site-main -->
+
+	</div> <!-- end #inner-content -->
+
+</div> <!-- end #content.contentarea-wrap -->
 
 <?php get_footer(); ?>
--- templates/with-sidebar.php
+++ templates/with-sidebar.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Full Width
+ * Template Name: With sidebar
  *
  * This is the full width page template, i.e. no sidebar
  *
@@ -9,22 +9,24 @@
 
 get_header(); ?>
 
-	<div id="content" class="contentarea-wrap">
-		
-		<div id="inner-content" class="row">
-	
-			<main id="main" class="site-main small-12 columns clearfix" role="main">
-		
-				<?php while ( have_posts() ) { the_post();
-	
-					get_template_part( 'tplparts/content', 'page' );
-	
-				} //endwhile; // end of the loop. ?>
-	
-			</main><!-- #main.site-main -->
-	
-		</div> <!-- end #inner-content -->
-	
-	</div> <!-- end #content.contentarea-wrap -->
+<div id="content" class="contentarea-wrap">
+
+	<div id="inner-content" class="row">
+
+		<main id="main" class="site-main small-12 medium-8 large-9 columns clearfix" role="main">
+
+			<?php while ( have_posts() ) { the_post();
+
+				get_template_part( 'tplparts/content', 'page' );
+
+			} //endwhile; // end of the loop. ?>
+
+		</main><!-- #main.site-main -->
+
+		<?php get_sidebar(); ?>
+
+	</div> <!-- end #inner-content -->
+
+</div> <!-- end #content.contentarea-wrap -->
 
 <?php get_footer(); ?>
